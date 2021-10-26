a0a7fd71d1da82ae9cc6bec7e6941ad975c2f652
Jason <thejasonmichaelwilliams@gmail.com>
2021-10-26 11:42:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const body = document.body;
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Jason is the champion";
+body.append(newHeader)
+
+
+
